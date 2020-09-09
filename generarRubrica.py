a38bce0b203f960ba824d994deeab8fc31f99999
ericksitocg <ericksitocg@hotmail.com>
2020-09-09 14:18:14 -0500

    agregando lectura escritura de json

--- generarRubrica.py
+++ generarRubrica.py
@@ -1,8 +1,9 @@
 import pyperclip as clipboard
 from playsound import playsound
 from time import sleep
+import json
 
-def crearDatos():
+def crearRubrica():
     temas = {}
     principal = ""
     c = 0
@@ -19,6 +20,8 @@ def crearDatos():
             opc = ingresaFormato()
         temas["Tema " + str(c)] = subtemas
         principal = input("\nIngresar nuevo tema? Enter: \nIngrese -1 para terminar el programa: ")
+    with open('rubrica.json', 'w') as fp:
+        json.dump(temas, fp)
     return temas
 
 def calificarRubrica(dic):
@@ -54,29 +57,54 @@ def ingresaFormato():
 
 def validaFormato(entrada):
     return  ("|" in entrada and entrada.count("|")== 1) or entrada== "-1"
-#a = crearDatos()
+"""
 rubrica = {
     "Tema1":{
-        "Parametros y variables de la funcion generaRetorno":15,
-        "Generacion de los archivos de nomina por cada ciudad":20
+        "Lectura csv":10,
+        "Ordenamiento de valores":10,
+        "Seleccion de columnas":5,
+        "Slicing de los 10 mayores":5,
+        "Creacion de grafico de barras":10
     },
     "Tema2":{
-    "Parametros y variables de la funcion generaRetorno":15,
-    "Generacion de los archivos de nomina por cada ciudad":20
+	    "Identificar a las filas de Ecuador":10,
+	    "Seleccionar las filas con magnitud mayor a 6":10,
+	    "Creacion de grafico de lineas":10
+	},
+    "Tema3":{
+	    "Seleccion de columnas type":10,
+	    "Contabilizar los valores":10,
+	    "Creacion de grafico de pastel":10
 	}
 }
+"""
+with open('rubrica.json') as json_file: 
+    rubrica = json.load(json_file) 
 
 print("Inicio proceso de calificacion de deberes")
-opcion = input("Desea calificar un deber ? enter|-1")
-while opcion != "-1":
-    calificarRubrica(rubrica)
+print("""
+	1)Crear rubrica
+	2)Presentar rubrica actual
+	3)Calificar usando la rubrica actual
+	""")
+op = input("Ingrese opcion:")
+if op == "1":
+	rubrica = crearRubrica()
+elif op == "2":
+	print(rubrica)
+elif op == "3":
+	opcion = input("Desea calificar un deber ? enter|-1")
+	while opcion != "-1":
+	    calificarRubrica(rubrica)
 
-    playsound("inicio.mp3")
-    sleep(10)
-    playsound("fin.mp3")
+	    playsound("inicio.mp3")
+	    sleep(10)
+	    playsound("fin.mp3")
 
-    opcion = input("Desea calificar un nuevo deber? enter|-1")
-print("Los deberes han sido calificados")
+	    opcion = input("Desea calificar un nuevo deber? enter|-1")
+	print("Los deberes han sido calificados")
+else:
+	print("opcion no valida")
 """
 clipboard.copy(corregido)
 playsound("inicio.mp3")
